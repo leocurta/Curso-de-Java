1d7096833d282e7bb75a2d33cb925eac3a410951
Leonardo Costa <leo.curta57@yahoo.com>
2024-08-06 20:08:07 -0400

    06/08

--- intPOO/comOrientacao.java
+++ intPOO/comOrientacao.java
@@ -2,8 +2,11 @@ package intPOO;
 
 import java.util.Scanner;
 import intPOO.entities.Triangulo;
+import java.util.Locale;
+
 public class comOrientacao {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         double p;
         Scanner sc = new Scanner(System.in);
 
@@ -16,16 +19,16 @@ public class comOrientacao {
         x.b = sc.nextDouble();
         x.c = sc.nextDouble();
         p = (x.a + x.b +x.c) / 2.0;
-        double ax = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+        x.area = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
        
         System.out.println("Entre com as medidas do triangulo y:");
         y.a = sc.nextDouble();
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
         p = (y.a + y.b +y.c) / 2.0;
-        double ay = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        y.area = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
-        System.out.printf("A area do triangulo x é: %.4f%n", ax);
-        System.out.printf("A area do triangulo y é: %.4f%n", ay);
+        System.out.printf("A area do triangulo x é: %.4f%n", x.area);
+        System.out.printf("A area do triangulo y é: %.4f%n", y.area);
     }
 }
--- intPOO/entities/Triangulo.java
+++ intPOO/entities/Triangulo.java
@@ -4,4 +4,5 @@ public class Triangulo {
     public double a;
     public double b;
     public double c;
+    public double area;
 }
